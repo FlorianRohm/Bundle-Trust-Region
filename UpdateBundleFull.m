aa634c4f78c84fec2edf722a8fd38ea1bb950681
Florian Rohm <r.florian@t-online.de>
2013-07-08 15:08:47 +0200

    Alternatives Bundleupdate

--- UpdateBundleFull.m
+++ UpdateBundleFull.m
@@ -4,6 +4,23 @@ function [ Bundle, Alphas ] = UpdateBundleFull(Bundle, Alphas, outcome, maxBundl
 currentSize = size(Bundle);
 currentSize = currentSize(2); % sadly no chaining :(
 
+
+sizeAfterUpdate = currentSize + 2;
+tooMuch = sizeAfterUpdate - maxBundleSize;
+
+if tooMuch >= 1
+   [~,index] = max(Alphas);
+   Bundle(:,index) = [];
+   Alphas(index) = [];
+   currentSize = currentSize-1;
+   if tooMuch == 2
+       [~,index2] = max(Alphas);
+       Bundle(:,index2) = [];
+       Alphas(index2) = [];
+       currentSize = currentSize-1;
+    end
+end
+
 Bundle(:,currentSize+1) = sTildeK;
 
 if outcome == -1
@@ -17,30 +34,6 @@ else
     
     Bundle(:,currentSize+2) = skPlus;
     Alphas(currentSize+2) = 0;
-end
-
-sizeAfterUpdate = currentSize + 2;
-tooMuch = sizeAfterUpdate - maxBundleSize;
-
-if tooMuch > 0
-    protected = 0;
-    for i = currentSize:-1:1
-        if Alphas(i) == 0
-            protected = i;
-            break;
-        end
-    end
-    
-    for i = 1:tooMuch
-        if 1 ~= protected
-            Bundle(:,1) = [];
-            Alphas(1) = [];
-        else
-            Bundle(:,2) = [];
-            Alphas(2) = [];
-        end
-    end
-    
 end
 
 end
